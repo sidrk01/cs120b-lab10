8c3888a5645aee002ce6849a361281e1f0add12e
sidrk01 <81598654+sidrk01@users.noreply.github.com>
2021-05-21 12:14:18 -0700

    Update main.c

--- Lab10_TaskScheduler/source/main.c
+++ Lab10_TaskScheduler/source/main.c
@@ -1,7 +1,7 @@
 /*	Author: sramk002
  *  Partner(s) Name: 
  *	Lab Section:
- *	Assignment: Lab #  Exercise #
+ *	Assignment: Lab #10  Exercise #1
  *	Exercise Description: [optional - include for your own benefit]
  *
  *	I acknowledge all content contained herein, excluding template or example
@@ -10,13 +10,31 @@
 #include <avr/io.h>
 #ifdef _SIMULATE_
 #include "simAVRHeader.h"
+#include "timer.h"
+#include "keypad.h"
 #endif
 
 int main(void) {
     /* Insert DDR and PORT initializations */
-
+        DDRB = 0xFF; PORTB = 0x00;
+        DDRC = 0xF0; PORTC = 0x0F;
     /* Insert your solution below */
+    unsigned char x;
     while (1) {
+        x = GetKeypadKey();
+        switch(x){
+            case '\0': PORTB = 0x1F; break; //All 5 LEDS On
+            case '1': PORTB = 0x01; break; //Hex equivalent
+            case '2': PORTB = 0x02; break;
+                
+            // .. ***** FINISH ***** 
+            
+            case 'D': PORTB = 0x0D; break;
+            case '*': PORTB = 0x0E; break;
+            case '0': PORTB = 0x00; break;
+            case '#': PORTB = 0x0F; break;
+            default: PORTB = 0x1B; break; //Should never occur. Middle LED off.
+        }
 
     }
     return 1;
