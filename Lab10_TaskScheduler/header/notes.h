d0856360a5b5706106abaff3ed567b274587db05
sidrk01 <81598654+sidrk01@users.noreply.github.com>
2021-05-23 16:52:12 -0700

    Update notes.h

--- Lab10_TaskScheduler/header/notes.h
+++ Lab10_TaskScheduler/header/notes.h
@@ -1,1 +1,45 @@
 #include "pwm.h"
+
+double freq[8] = {261.63, 293.66, 261.63, 349.23, 261.63};
+unsigned int index
+enum Note_States { Cont, Play, Stop } notes;
+
+int Tick_Fct3(int state){
+  switch(state){
+    case Cont:
+      index = 0;
+       if (button2){
+        state = Play;
+         PWM_on()
+       } else {
+        state = Cont;
+       }
+      break;
+      
+    case Play:
+      if (index < 7){
+        index += 1;
+        state = Play;
+      } else if (index > 7){
+        state = Stop;
+      }
+      break;
+      
+    case Stop:
+     state = Cont;
+      break;
+  }
+  
+  switch (state){
+    case Cont:
+      break;
+      
+    case Wait:
+      set_PWM(freq[index]);
+     break;
+      
+    case Stop:
+      PWM_off();
+     break;
+  }
+}
